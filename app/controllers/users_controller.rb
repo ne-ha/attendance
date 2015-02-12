34812125156e3e7623d1714b68dd70f5f4cda22c
NEHA SUWAl <neha@jyaasa.com>
2015-02-12 14:53:20 +0545

    added actions such as index , new , create, edit , delete

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,23 +1,46 @@
 class UsersController < ApplicationController
 
   layout false
+
+  before_action :authenticate_user!
   
+  def index
+    @users = User.all
+  end
+
   def new
-  end
-
-  def edit
-  end
-
-  def delete
+    @user = User.new
   end
 
   def create
+    @user = User.new(user_params)
+    if @user.save
+      redirect_to(:action => 'index')
+    else
+      render("new")
+    end
   end
 
-  def index
+  def edit
+    @user = User.find(params[:id])
   end
 
-  def show
+  def update
+    @user = User.find(params[:id])
+    if @user.update_attributes(user_params)
+      redirect_to(:action => 'index')
+    else
+      render("edit")
+    end
+  end
+
+  def delete
+    @user = User.find(params[:id])
+  end
+
+  def destroy
+    User.find(params[:id]).destroy
+    redirect_to(:action => 'index')
   end
 
 end
