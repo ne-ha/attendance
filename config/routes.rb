cce21be2c3746c053b4891d4fcdad0575b4a4ccf
NEHA SUWAl <neha@jyaasa.com>
2015-02-12 10:55:36 +0545

    resources are added

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,12 @@
 Rails.application.routes.draw do
   
-  match ':controller(/:action(:/id))' , :via => :get
+  # match ':controller(/:action(:/id))' , :via => :get
 
+  resources :users do
+    member do
+      get :delete
+    end
+  end
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
