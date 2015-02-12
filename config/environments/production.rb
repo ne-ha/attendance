2a169701bdd2a82ad97f2041c06729a460b9c9c5
NEHA SUWAl <neha@jyaasa.com>
2015-02-12 17:22:55 +0545

    added quotes for port

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -82,7 +82,7 @@ Rails.application.configure do
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.smtp_settings = {
     :address => "127.0.0.1",
-    :port    => 25,
+    :port    => "25",
     :domain  => 'yourdomain.com'
   }
 
