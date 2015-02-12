f467608ec9d069bb62e71ca11eaec3b1125cfd26
NEHA SUWAl <neha@jyaasa.com>
2015-02-12 15:27:49 +0545

    added a default for username

--- db/migrate/20150212054042_create_users.rb
+++ db/migrate/20150212054042_create_users.rb
@@ -4,7 +4,7 @@ class CreateUsers < ActiveRecord::Migration
     create_table :users do |t|
       t.string "first_name", :limit => 25
       t.string "last_name", :limit => 50
-      t.string "username", :limit => 25, :null => false
+      t.string "username", :limit => 25, :default => ""
       t.string "password" , :limit => 40
       t.string "role"
       t.timestamps null: false
