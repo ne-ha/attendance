c4f0b3f44bc269238b4cf4e85d10e226571b55e0
NEHA SUWAl <neha@jyaasa.com>
2015-02-12 17:22:34 +0545

    added quotes for the port number

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -45,5 +45,5 @@ Rails.application.configure do
   # for sending confirmation email from devise
   config.action_mailer.default_url_options = { :host => 'localhost:3000' }
   config.action_mailer.delivery_method = :smtp
-  config.action_mailer.smtp_settings = {:address => "localhost", :port => 1025}
+  config.action_mailer.smtp_settings = {:address => "localhost", :port => "1025"}
 end
